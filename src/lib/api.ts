0f97a76a87bcb650a89685975370af726b29dd83
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-07-13 16:07:45 +0100

    Add api stuff

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,5 @@
+import { PUBLIC_APP_API_LOC } from '$env/static/public'
+
 export enum CorrectMode {
     GOOD,
     BAD,
@@ -44,5 +46,57 @@ export type Section = SectionSlide | SectionQuestion | SectionFinale
 export type SectionResp = {
     next?: Section,
     transitionMode: CorrectMode
+}
+
+export function apiCtx(fetchFunc = fetch, url = new URL(location as unknown as string)): { fetch: typeof fetch, url: URL } {
+    return {
+        fetch: fetchFunc,
+        url,
+    }
+}
+
+async function apiCall(method: string, path: string, data?: Record<string, unknown>, { fetch } = apiCtx()): Promise<Record<string, unknown>> {
+    const resp = await fetch(`${PUBLIC_APP_API_LOC ?? "/api"}${path}`, {
+        method,
+        body: data ? JSON.stringify(data) : "{}",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    let body: Record<string, unknown> | null
+
+    try {
+        body = await resp.json()
+    } catch {
+        console.error("Failed to parse body as JSON!")
+        body = null
+    }
+    
+    if (!body || typeof body != "object") {
+        throw `Unexpected body: (status: ${resp.status})`
+    }
+
+    if (resp.status == 200) {
+        return body
+    }
+
+    if (body.error) {
+        throw `API Exception: ${body.error}`
+    }
+
+    throw `Unknown API Exception: ${resp.status}`
+}
+
+function getKey(url: URL) {
+    return (new URLSearchParams(url.search)).get("key")
+}
+
+export async function nextSection(currentSection: string, answer = "", ctx = apiCtx()): Promise<SectionResp> {
+    return apiCall("POST", "/", { currentSection, answer, key: getKey(ctx.url) }, ctx) as Promise<SectionResp>
+}
+
+export async function getFinale(secret: string, ctx = apiCtx()): Promise<FinaleInfo> {
+    return apiCall("POST", "/finale", { finaleSecret: secret }, ctx) as Promise<FinaleInfo>
 }
 
