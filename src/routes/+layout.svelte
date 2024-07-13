f1cd1c210f510c44cca6905a0846f8841279c424
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-07-13 16:07:34 +0100

    Basic layouting

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import "../app.scss";
+	import '@fontsource/roboto';
+</script>
+
+<div class="container col">
+    <slot />
+</div>
+
+<style lang="scss">
+    .container {
+        height: 100dvh;
+    }
+</style>
