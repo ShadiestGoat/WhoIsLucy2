e1215742c9f570f0cd24640c0c8abd0ce5e33369
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-07-15 00:43:25 +0100

    extract btn into separate component

---
+++ src/lib/btn.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import { shouldContinueEvent, type AnyInputEvent } from "./input";
+
+    const dispatch = createEventDispatcher<{ submit: null }>()
+    function submit(e: AnyInputEvent) {
+		if (!shouldContinueEvent(e, true)) {
+			return
+		}
+
+        dispatch('submit', null)
+	}
+
+    export let text: string
+    export let autofocus = true
+</script>
+
+<!-- svelte-ignore a11y-autofocus -->
+<button
+    class="input"
+    type="button"
+    tabindex="0"
+    {autofocus}
+    on:click={submit}
+    on:keypress={submit}
+>
+    <p>{text}</p>
+</button>
+
+<style lang="scss">
+    @use './scss/input';
+
+    p {
+        font-size: 1rem;
+    }
+</style>
--- src/lib/sections/slide.svelte
+++ src/lib/sections/slide.svelte
@@ -1,43 +1,20 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte";
 	import type { SectionSlide } from "../api";
-	import { shouldContinueEvent, type AnyInputEvent } from "$lib/input";
+	import Btn from "$lib/btn.svelte";
 
     const dispatch = createEventDispatcher<{ next: null }>()
     export let data: SectionSlide
-
-    function submit(e: AnyInputEvent) {
-		if (!shouldContinueEvent(e, true)) {
-			return
-		}
-
-        dispatch('next', null)
-	}
 </script>
 
 {#if data.slide.subTitle}
     <h2 class="heading">{data.slide.subTitle}</h2>
 {/if}
 
-<!-- svelte-ignore a11y-autofocus -->
-<button
-    class="input"
-    type="button"
-    tabindex="0"
-    autofocus
-    on:click={submit}
-    on:keypress={submit}
->
-    <p>{data.slide.nextText}</p>
-</button>
+<Btn text={data.slide.nextText} on:submit={() => dispatch('next', null)} />
 
 <style lang="scss">
-    @use "../scss/input";
-
     h2 {
         margin-top: 3vh;
-    }
-    p {
-        font-size: 1rem;
     }
 </style>
